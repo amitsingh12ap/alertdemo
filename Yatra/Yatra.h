657b6f796aa07c91256e5db06022c54f0c6dcd64
Amit <amit.singh@yatra.com>
2019-02-26 21:18:01 +0530

    Initial Commit

---
+++ Yatra/Yatra.h
@@ -0,0 +1,19 @@
+//
+//  Yatra.h
+//  Yatra
+//
+//  Created by Amit Singh on 26/02/19.
+//  Copyright © 2019 Amit Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Yatra.
+FOUNDATION_EXPORT double YatraVersionNumber;
+
+//! Project version string for Yatra.
+FOUNDATION_EXPORT const unsigned char YatraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yatra/PublicHeader.h>
+
+
---
+++ YatraTests/YatraTests.swift
@@ -0,0 +1,34 @@
+//
+//  YatraTests.swift
+//  YatraTests
+//
+//  Created by Amit Singh on 26/02/19.
+//  Copyright © 2019 Amit Singh. All rights reserved.
+//
+
+import XCTest
+@testable import Yatra
+
+class YatraTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
