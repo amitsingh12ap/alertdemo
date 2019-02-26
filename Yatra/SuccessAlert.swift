7aa69544f0e14fe3e3c645e5b845ef255c7a52ba
Amit <amit.singh@yatra.com>
2019-02-26 21:52:50 +0530

    testing done

--- Yatra/SuccessAlert.swift
+++ Yatra/SuccessAlert.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
-class SuccessAlert: NSObject {
+public class SuccessAlert: NSObject {
+    public static func showSuccessAlert(_ controller : UIViewController){
+        let alertController = UIAlertController(title: "Alert", message: "This is an alert.", preferredStyle: .alert)
+        
+        let action1 = UIAlertAction(title: "Default", style: .default) { (action:UIAlertAction) in
+            print("You've pressed default");
+        }
+        
+        let action2 = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction) in
+            print("You've pressed cancel");
+        }
+        
+        let action3 = UIAlertAction(title: "Destructive", style: .destructive) { (action:UIAlertAction) in
+            print("You've pressed the destructive");
+        }
+        
+        alertController.addAction(action1)
+        alertController.addAction(action2)
+        alertController.addAction(action3)
+        controller.present(alertController, animated: true, completion: nil)
 
+    }
 }
