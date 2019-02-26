f1808984c84b67134acf909b55fb69aade05a0fc
Amit <amit.singh@yatra.com>
2019-02-26 21:41:26 +0530

    first commit

---
+++ Yatra/SuccessAlert.swift
@@ -0,0 +1,13 @@
+//
+//  SuccessAlert.swift
+//  Yatra
+//
+//  Created by Amit Singh on 26/02/19.
+//  Copyright © 2019 Amit Singh. All rights reserved.
+//
+
+import UIKit
+
+class SuccessAlert: NSObject {
+
+}
